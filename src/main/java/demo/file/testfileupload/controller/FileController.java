2dae43ffebca03e9b1ecfa9a7d43903f552a26e6
root <nagarajan10102000@gmail.com>
2023-08-29 19:44:53 +0530

    filedownload

--- src/main/java/demo/file/testfileupload/controller/FileController.java
+++ src/main/java/demo/file/testfileupload/controller/FileController.java
@@ -3,9 +3,12 @@ package demo.file.testfileupload.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,13 +29,24 @@ public class FileController {
     }
 
     @PostMapping("/upload")
-    @ResponseBody
     public String processupload(@RequestParam("file") MultipartFile file,Model model) throws IOException{
         byte[] content = file.getBytes();
         String name = file.getName();
         String filetype = file.getContentType();
         fileupload Fileupload = new fileupload(0, content, name, filetype);
         fileService.saveFile(Fileupload);
-        return "filetype";
+        model.addAttribute("filedownload", fileService.getAllFiles());
+        return "Downloadfile";
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<byte[]> getfiles(@PathVariable Long id){
+        fileupload file = fileService.getFiles(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE,file.getFiletype());
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachements; filename="+file.getFiletype());
+        return ResponseEntity.ok()
+        .headers(headers)
+        .body(file.getContent());
     }
 }
